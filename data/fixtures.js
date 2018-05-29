0c7957c2d10fec1fe4b3f3034012c9488f84950a
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-05-29 10:46:12 -0600

    initial commit

---
+++ data/fixtures.js
@@ -0,0 +1,22 @@
+module.exports = {
+  yeast: [{
+    id: 1,
+    type: 'Glazed Doughnuts'
+  },
+  {
+    id: 2,
+    type: 'Twist Doughnuts'
+  },
+  {
+    id: 3,
+    type: 'Jelly Doughnuts'
+  },
+  {
+    id: 4,
+    type: 'Cream filled doughnuts'
+  },
+  {
+    id: 5,
+    type: 'Dutchie'
+  }
+  ]}
